198e5c30440e38d9d9054bbd8301166a7108296a
Cholo0022 <andres.bozzani@gmail.com>
2021-11-15 16:33:02 -0300

    Section card creation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Container from "./components/Container";
+import Section from "./components/Section";
 
 function App() {
-  return <Container>jajaja</Container>;
+  return (
+    <Container>
+      <Section>jajajaj</Section>
+    </Container>
+  );
 }
 
 export default App;
